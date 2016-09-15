dcd9136f5e966ee96abb88270eeafc4ef53237a7
Sushant Magoo <sushantmagoo@gmail.com>
2016-09-16 00:46:52 +0530

    euler3

--- Python/Euler3.py
+++ Python/Euler3.py
@@ -1,18 +1,13 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
-#What is the largest prime factor of the number 600851475143 ?
+# What is the largest prime factor of the number 600851475143?
 
 def euler3(n):
-
     while n % 2 == 0 and n != 2:
     	n = n/2
-
     i = 3
-
     while i < n ** 0.5:
-
         if n % i == 0:
             n = n / i
-
         i = i + 2
     return n
             
